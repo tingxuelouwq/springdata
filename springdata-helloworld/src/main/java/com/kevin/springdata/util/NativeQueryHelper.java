4fa0963621e968122e3786c519387859af57d464
tingxuelouwq <kevin_wangqi@126.com>
2023-06-18 21:48:49 +0800

    泛型native sql

--- springdata-helloworld/src/main/java/com/kevin/springdata/util/NativeQueryHelper.java
+++ springdata-helloworld/src/main/java/com/kevin/springdata/util/NativeQueryHelper.java
@@ -29,23 +29,24 @@ public class NativeQueryHelper {
 
     /**
      * 不带查询参数的原生SQL动态查询
-     * @param sql           原生SQL
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              结果集列表
+     *
+     * @param sql         原生SQL
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 结果集列表
      */
     public <T> List<T> nativeQuery(String sql, Class<T> resultClass) {
         return nativeQuery(sql, null, resultClass);
     }
 
     /**
-     *
      * 带查询参数的原生SQL动态查询
-     * @param sql           原生SQL
-     * @param param         查询参数
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              结果集列表
+     *
+     * @param sql         原生SQL
+     * @param param       查询参数
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 结果集列表
      */
     public <T> List<T> nativeQuery(String sql, Map<String, Object> param, Class<T> resultClass) {
         Query nativeQuery = entityManager.createNativeQuery(sql);
@@ -61,23 +62,24 @@ public class NativeQueryHelper {
 
     /**
      * 不带查询参数的原生SQL查询
-     * @param sql           原生SQL
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              单个结果实体
+     *
+     * @param sql         原生SQL
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 单个结果实体
      */
     public <T> T nativeQuerySingleResult(String sql, Class<T> resultClass) {
         return nativeQuerySingleResult(sql, null, resultClass);
     }
 
     /**
-     *
      * 带查询参数的原生SQL动态查询
-     * @param sql           原生SQL
-     * @param param         查询参数
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              单个结果实体
+     *
+     * @param sql         原生SQL
+     * @param param       查询参数
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 单个结果实体
      */
     public <T> T nativeQuerySingleResult(String sql, Map<String, Object> param, Class<T> resultClass) {
         Query nativeQuery = entityManager.createNativeQuery(sql);
@@ -93,58 +95,60 @@ public class NativeQueryHelper {
 
     /**
      * 不带查询参数的原生SQL动态查询，不带计数SQL(建议自行定义计数SQL，默认的计数SQL使用了子查询，性能上有折扣)
-     * @param sql           原生SQL
-     * @param pageable      分页参数
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              结果集分页列表
+     *
+     * @param sql         原生SQL
+     * @param pageable    分页参数
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 结果集分页列表
      */
     public <T> Page<T> nativeQueryPage(String sql, Pageable pageable, Class<T> resultClass) {
-        String countSql = "SELECT COUNT(*)  FROM ( "+ sql +" ) getcount";
+        String countSql = "SELECT COUNT(*)  FROM ( " + sql + " ) getcount";
         return nativeQueryPage(sql, countSql, null, pageable, resultClass);
     }
 
     /**
-     *
      * 带查询参数的原生SQL动态查询，不带计数SQL(建议自行定义计数SQL，默认的计数SQL使用了子查询，性能上有折扣)
-     * @param sql           原生SQL
-     * @param param         查询参数
-     * @param pageable      分页参数
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              结果集分页列表
+     *
+     * @param sql         原生SQL
+     * @param param       查询参数
+     * @param pageable    分页参数
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 结果集分页列表
      */
     public <T> Page<T> nativeQueryPage(String sql, Map<String, Object> param, Pageable pageable, Class<T> resultClass) {
-        String countSql = "SELECT COUNT(*)  FROM ( "+ sql +" ) getcount";
+        String countSql = "SELECT COUNT(*)  FROM ( " + sql + " ) getcount";
         return nativeQueryPage(sql, countSql, param, pageable, resultClass);
     }
 
     /**
      * 不带查询参数的原生SQL动态查询
-     * @param sql           原生SQL
-     * @param countSql      计数SQL
-     * @param pageable      分页参数
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              结果集分页列表
+     *
+     * @param sql         原生SQL
+     * @param countSql    计数SQL
+     * @param pageable    分页参数
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 结果集分页列表
      */
     public <T> Page<T> nativeQueryPage(String sql, String countSql, Pageable pageable, Class<T> resultClass) {
         return nativeQueryPage(sql, countSql, null, pageable, resultClass);
     }
 
     /**
-     *
      * 带查询参数的原生SQL动态查询
-     * @param sql           原生SQL
-     * @param countSql      计数SQL
-     * @param param         查询参数
-     * @param pageable      分页参数
-     * @param resultClass   结果集实体类对应的Class
-     * @param <T>           结果集实体类
-     * @return              结果集分页列表
+     *
+     * @param sql         原生SQL
+     * @param countSql    计数SQL
+     * @param param       查询参数
+     * @param pageable    分页参数
+     * @param resultClass 结果集实体类对应的Class
+     * @param <T>         结果集实体类
+     * @return 结果集分页列表
      */
     public <T> Page<T> nativeQueryPage(String sql, String countSql, Map<String, Object> param, Pageable pageable, Class<T> resultClass) {
-        Query nativeQuery=entityManager.createNativeQuery(sql);
+        Query nativeQuery = entityManager.createNativeQuery(sql);
         Query nativeCountQuery = entityManager.createNativeQuery(countSql);
 
         if (!CollectionUtils.isEmpty(param)) {
